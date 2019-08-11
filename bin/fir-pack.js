86d3ca800bcd94e8dd39026bbc54d331626b664e
firyang <firyang@aliyun.com>
2019-08-11 16:57:20 +0800

    实现Compiler

--- bin/fir-pack.js
+++ bin/fir-pack.js
@@ -2,11 +2,10 @@
 
 let path = require('path')
 
-// config配置文件
 let config = require(path.resolve('webpack.config.js'))
 
 let Compiler = require('../lib/Compiler.js')
+
 let compiler = new Compiler(config)
 
-// 标识运行编译
 compiler.run()
